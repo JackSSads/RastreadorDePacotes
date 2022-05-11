994132a3c63f93d8b0c1280311a958ea76ccb6e3
JackSSads <fotosjackson2018@hotmail.com>
2022-05-11 01:17:55 -0300

    Primary Commit

---
+++ RastreadorDePacotes/RastreadorDePacotes/Dados.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace RastreadorDePacotes
+{
+    class Dados
+    {
+        public string Cep { get; set; }
+        public string Logradouro { get; set; }
+        public string Complemento { get; set; }
+        public string Bairro { get; set; }
+        public string Localidade { get; set; }
+        public string UF { get; set; }
+        public string Ibge { get; set; }
+        public string Ddd { get; set; }
+        public string Siafi { get; set; }
+
+        public string JsonSerializar(Dados dados)
+        {
+            return JsonConvert.SerializeObject(dados);
+        }
+
+        public static Dados JsonDesserializar(string Json)
+        {
+            return JsonConvert.DeserializeObject<Dados>(Json);
+        }
+    }
+}
---
+++ RastreadorDePacotes/RastreadorDePacotes/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Correios.NET;
+
+namespace RastreadorDePacotes
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private void btnBuscar_Clicked(object sender, EventArgs e)
+        {
+            var pacote = "JN709355478BR";//etInsert.Text;
+            _ = RastrearPacoteAsync(pacote);
+        }
+        public async Task RastrearPacoteAsync(string pacote)
+        {
+            var result = await new CorreiosService().GetPackageTrackingAsync(pacote);
+
+            foreach (var track in result.TrackingHistory)
+            {
+                //lblHora.Text = $"Etregue em {Convert.ToString(track.Date)}";
+
+                lblPostado.Text = "Postado em:";
+                lblRPostado.Text = $"{track.Source} {Convert.ToString(track.Date)}";
+                //lblDestino.Text = $"{track.Destination}";
+                lblStatus.Text = $"{track.Status}";
+            }
+        }
+    }
+}
